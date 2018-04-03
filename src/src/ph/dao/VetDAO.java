98d491e0b4e90890815bc40a655ba6503ec1b385
hlzhangwhvcse <25538330@qq.com>
2018-04-03 10:03:17 +0800

    VetDAO增加了根据ID删除医生记录的方法delete(int VetId), VetServlet的doGet()中增加了处理删除医生的代码，已完成根据ID删除医生的功能， by hlzhang 20180403 at 504

--- src/src/ph/dao/VetDAO.java
+++ src/src/ph/dao/VetDAO.java
@@ -188,4 +188,47 @@ public class VetDAO
     }
 
 
+    /**
+     * 根据医生的Id，删除医生表中的对应记录行
+     * @param vetID
+     *            vetID医生Id号
+     * @return void
+     * @throws Exception
+     */
+    public void delete(int vetId) throws Exception
+    {
+        Connection con = null;
+        PreparedStatement ps = null;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_ph","root","123456");//  协议://域名(ip):端口/资源（数据库名）
+            ps = con.prepareStatement("delete from t_vet where id=?");
+            ps.setInt(1, vetId);
+            ps.executeUpdate();
+        }
+        catch(ClassNotFoundException e)
+        {
+            e.printStackTrace();
+            throw new Exception("找不到驱动:"+e.getMessage());//异常不能在底层丢失了
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            throw new Exception("数据库操作错误:"+e.getMessage());
+        }
+        finally
+        {
+            if(ps!=null)
+            {
+                ps.close();
+            }
+            if(con!=null)
+            {
+                con.close();
+            }
+        }
+    }
+
+
 }
