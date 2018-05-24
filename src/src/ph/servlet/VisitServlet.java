fd8144e65bb76cdf695cb1760325918b31a72dfc
hlzhangwhvcse <25538330@qq.com>
2018-05-24 09:19:41 +0800

    1)病历管理中修改为能根据宠物名字和宠物主人姓名查询宠物信息 2)增加了病历实体类Visit和病历数据库访问类VisitDAO历史病历页面查询历史病历 3）增加了显示历史病历的页面showCaseHistory.jsp 4)VisitServlet增加了查询历史病历的方法showCaseHistory

--- src/src/ph/servlet/VisitServlet.java
+++ src/src/ph/servlet/VisitServlet.java
@@ -13,9 +13,9 @@ import java.util.List;
 import src.ph.dao.PetDAO;
 import src.ph.dao.VetDAO;
 import src.ph.po.Pet;
-//import src.ph.dao.VisitDAO;
+import src.ph.dao.VisitDAO;
 import src.ph.po.Vet;
-//import src.ph.po.Visit;
+import src.ph.po.Visit;
 
 //@WebServlet(name = "VisitServlet")
 @WebServlet("/VisitServlet")
@@ -33,15 +33,20 @@ public class VisitServlet extends HttpServlet
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-
+        String mode = request.getParameter("mode");
+        if ("showCaseHistory".equals(mode))//浏览病历
+        {
+            showCaseHistory(request, response);
+        }
     }
 
     private void searchPet(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException
     {
         String petName = request.getParameter("petName");
+        String ownerName = request.getParameter("ownerName");
         try
         {
-            List<Pet> pets = new PetDAO().search(petName, "");
+            List<Pet> pets = new PetDAO().search(petName, ownerName);
             if(0==pets.size())
             {
                 request.setAttribute("msg", "没有查到宠物信息");
@@ -57,6 +62,26 @@ public class VisitServlet extends HttpServlet
         {
             request.setAttribute("msg", e.getMessage());
             request.getRequestDispatcher("/visitSearch.jsp").forward(request, response);
+        }
+    }
+
+    private void showCaseHistory(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        try
+        {
+            VisitDAO visitDAO = new VisitDAO();
+            List<Visit> visits = visitDAO.getVisitsByPetId(Integer.parseInt(request.getParameter("petId")));
+            request.setAttribute("visits", visits);
+            if (0 == visits.size())
+            {
+                request.setAttribute("msg", "没有找到历史病历");
+            }
+            request.getRequestDispatcher("/showCaseHistory.jsp").forward(request, response);
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/showCaseHistory.jsp").forward(request, response);
         }
     }
 }
